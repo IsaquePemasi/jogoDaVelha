acac0fa52088af84e20ca9859faeb7221f5aa005
IsaquePemasi <ADSisaque@gmail.com>
2023-04-18 18:34:55 -0300

    first commit

---
+++ jogoDaVelha/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace jogoDaVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matriz = new string[3, 3];
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = "0";
+                }
+            }
+            //imprimeindo a matriz,($" [{matriz[i, j]}] ")
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($" [{matriz[i, j]}] ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
